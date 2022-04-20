6bc97d8a1be0c63deb8988d8e3af2be5716e17c7
shuto0319 <shuto.y0319@gmail.com>
2022-04-20 07:23:32 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin, controllers: {
+  registrations: "admin/registrations",
+  sessions: "admin/sessions"
+}
+  devise_for :customers,controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
